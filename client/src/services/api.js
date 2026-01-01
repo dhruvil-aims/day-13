990ecff8d2722669cfc760499360216c2aaca5e6
dhruvil-aims <dhruvil.patel@Aimsinfosoft.com>
2026-01-01 15:35:15 +0530

    Second commits

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,10 +3,15 @@ import axios from "axios";
 // const API = axios.create({
 //   baseURL: "http://localhost:5000", // your backend
 // });
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true,
+// });
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://day-13.onrender.com/",
   withCredentials: true,
 });
+
 // axios.post(`${import.meta.env.VITE_API_URL}/auth/login`);
 
 
