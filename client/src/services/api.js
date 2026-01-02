451082d946c49d50240a4a295e6c4f7f28355190
dhruvil-aims <dhruvil.patel@Aimsinfosoft.com>
2026-01-02 11:40:52 +0530

    remove withcredentials

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // });
 const API = axios.create({
   baseURL: "https://day-13.onrender.com",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 // axios.post(`${import.meta.env.VITE_API_URL}/auth/login`);
