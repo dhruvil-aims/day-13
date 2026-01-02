23c5f12c8bf32a59115ddbd6c4c60c13bb357830
dhruvil-aims <dhruvil.patel@Aimsinfosoft.com>
2026-01-02 18:39:03 +0530

    add cloudinary data

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,9 +1,14 @@
 import { v2 as cloudinary } from "cloudinary";
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "dg3ncqeqh",
+  api_key: "874488624476642",
+  api_secret: "8BS8oPG0bXnjECJKYauHPqmyO5Q",
 });
 
 export default cloudinary;
